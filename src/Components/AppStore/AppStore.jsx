1a29531366883490cd6c66d5e56e98f55153059e
enwelum chiamaka <enwelumchiamaka@gmail.com>
2024-03-21 13:56:56 +0100

    third commit

--- src/Components/AppStore/AppStore.jsx
+++ src/Components/AppStore/AppStore.jsx
@@ -18,7 +18,9 @@ const AppStore = () => {
     <div style={backgroundStyle} className='py-14'>
       <div className="container">
         <div className="grid grid-cols-2 sm:grid-cols-2 items-center gap-4">
-            <div className='space-y-6 max-w-xl mx-auto'>
+            <div
+                data-aos="fade-up"
+                className='space-y-6 max-w-xl mx-auto'>
                 {/* text Content*/}
                 <h1 className='text-2xl text-center sm:text-left sm:text-4xl font-semibold pl-3 text-white/90'>
                     Cafie Cofe is available fro Android and IOS
--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -22,32 +22,56 @@ const Banner = () => {
             <div className='container min-h-[550px] flex justify-center items-center py-12 sm:py-0'>
                 <div className='grid grid-cols-1 sm:grid-cols-2 gap-6'>
                     {/*Image Section */}
-                    <div>
-                        <img src={BannerImg} alt="" className='max-w-[430px] w-full mx-auto spin drop-shadow-xl'/>
+                    <div data-aos="zoom-in">
+                        <img 
+                            src={BannerImg} 
+                            alt="" 
+                            className='max-w-[430px] w-full mx-auto spin drop-shadow-xl'
+                        />
                     </div>
                     {/*text Content Section */}
                     <div className='flex flex-col justify-center gap-6 sm:pt-0'>
-                        <h1 className='text-3xl sm:text-4xl font-bold font-cursive'>Premium Blen Coffee</h1>
-                        <p className='text-sm text-gray-500 tracking-wider leading-5'>
+                        <h1 
+                            data-aos="fade-up"
+                            className='text-3xl sm:text-4xl font-bold font-cursive'>Premium Blen Coffee
+                        </h1>
+                        <p 
+                            dat-aos="fade-up"
+                            className='text-sm text-gray-500 tracking-wider leading-5'
+                        >
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem quasi tempora a vero distinctio? Quibusdam odio laborum temporibus eius adipisci animi illum quia corrupti nesciunt molestiae similique, molestias fugit at?
                         </p>
                         <div className='grid grid-cols-2 gap-6 '>
                             <div className='space-y-5'>
-                                <div className='flex items-center gap-3'>
+                                <div data-aos="fade-up" className='flex items-center gap-3'>
                                     <GrSecure className='text-2xl h-12 w-12 shadow-sm p-3 rounded-full bg-red-100'/>
                                     <span>Premium Coffee</span>
                                 </div>
-                                <div className='flex items-center gap-3'>
+                                <div
+                                    data-aos="fade-up"
+                                    data-aos-delay="500" 
+                                    className='flex items-center gap-3'
+                                >
                                     <IoFastFood className='text-2xl h-12 w-12 shadow-sm p-3 rounded-full bg-orange-100'/>
                                     <span>Hot Coffee</span>
                                 </div>
-                                <div className='flex items-center gap-3'>
+                                <div 
+                                    data-aos="fade-up"
+                                    data-aos-delay="500"
+                                    data-aos-offset="0"
+                                    className='flex items-center gap-3'
+                                >
                                     <GiFoodTruck className='text-2xl h-12 w-12 shadow-sm p-3 rounded-full bg-yellow-100'/>
                                     <span>Cold Coffee</span>
                                 </div>
                             </div>
-                            <div className='border-l-4 border-primary/50 pl-6  space-y-3'>
-                                <h1 className='text-2xl font-semibold font-cursive'>Tea Lover</h1>
+                            <div 
+                                data-aos="slide-left"
+                                className='border-l-4 border-primary/50 pl-6  space-y-3'
+                            >
+                                <h1 className='text-2xl font-semibold font-cursive'>
+                                    Tea Lover
+                                </h1>
                                 <p className='text-gray-code text-sm'>
                                     {" "}
                                     Much like writing code, brewing the prefect cup of tea requires patience, 
